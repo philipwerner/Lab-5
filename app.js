e71d6294cfa5035fa3aeda37fc45a32ac0e37a08
philipwerner <philip.r.werner@gmail.com>
2017-07-14 12:09:06 -0700

    notated problem one completed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function sum(a,b){ //eslint-disable-line
 var result = a + b;
 var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
