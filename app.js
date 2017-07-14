015ccf6fc6cc683d4393912df52ed2f3a9d14371
philipwerner <philip.r.werner@gmail.com>
2017-07-14 13:49:52 -0700

    solved problem 3

--- app.js
+++ app.js
@@ -45,13 +45,17 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// DONE: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+var sum = a + b + c;
+var product = a * b * c;
+var messageSum = a + ' and ' + b + ' and ' + c + ' sum to ' + sum + '.';
+var messageProduct = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.';
+return [sum, product, messageSum, messageProduct];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
